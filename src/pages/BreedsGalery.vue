c37383940326844858aa88a0c081dbe83f80ec5e
Ramon Ribeiro Noleto <ramon.zap@hotmail.com>
2024-05-20 23:45:46 -0300

    Galeria de Raças
    Retorna imagem correta para a raça

--- src/pages/BreedsGalery.vue
+++ src/pages/BreedsGalery.vue
@@ -5,7 +5,7 @@
       <div class="galery">
         <div v-for="(breed, index) in breedsList" :key="index" class="card">
           <h3 class="text">{{ breed.breed }}</h3>
-          <img :src="getBreedImage(breed.id)" :alt="breed.breed">
+          <img :src="getBreedImage(breed.id)" alt="imagem aqui">
         </div>
       </div>
     </div>
@@ -15,65 +15,17 @@
 <script>
 import axios from 'axios';
 
-
 export default {
   data() {
     return {
-      breedsList: [
-        // {
-        //   id: 1,
-        //   breed: "Poodle",
-        // },
-        // {
-        //   id: 2,
-        //   breed: "Maltes",
-        // },
-        // {
-        //   id: 3,
-        //   breed: "Vira Lata",
-        // },
-        // {
-        //   id: 4,
-        //   breed: "Dog Alemão",
-        // },
-        // {
-        //   id: 5,
-        //   breed: "Pincher",
-        // }
-      ],
-      images: [
-        //   {
-        //     id: 1,
-        //     ref_id_breed: 1,
-        //     img: "https://images.unsplash.com/photo-1555596873-1916fae19257?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9vZGxlfGVufDB8fDB8fHww",
-        //   },
-        //   {
-        //     id: 2,
-        //     ref_id_breed: 2,
-        //     img: "https://media.istockphoto.com/id/172653881/pt/foto/cachorrinho.webp?b=1&s=170667a&w=0&k=20&c=qOihZOCUM8Gow-2nfZEj-xwKGTB0ewNJ8H0fO0NKfBo=",
-        //   },
-        //   {
-        //     id: 3,
-        //     ref_id_breed: 3,
-        //     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUvfALBt1sP8XV2SVtDik2r9BAnxWZ1MMVHw&s",
-        //   },
-        //   {
-        //     id: 4,
-        //     ref_id_breed: 4,
-        //     img: "https://media.istockphoto.com/id/546202048/pt/foto/great-dane-on-white-background.webp?b=1&s=170667a&w=0&k=20&c=TfKZHaq6rKMCFWtCA8wXItEKrdiVXOx7WNWvbLwH8ss=",
-        //   },
-        //   {
-        //     id: 5,
-        //     ref_id_breed: 5,
-        //     img: "https://media.istockphoto.com/id/177769631/pt/foto/senior-pinscher-an%C3%A3o.webp?b=1&s=170667a&w=0&k=20&c=AYNVCPFmEJMkDJFMIMII0DFhI7W3kSVZKunsuq8EFoY=",
-        //   }
-      ]
+      breedsList: [],
+      images: []
     }
   },
   methods: {
     async loadBreeds() {
       try {
-        const response = await axios.get("/breeds");
+        const response = await axios.get("breeds");
         this.breedsList = response.data;
       } catch (error) {
         console.error("Erro ao carregar Raças", error);
@@ -81,22 +33,24 @@ export default {
     },
     async loadImages() {
       try {
-        for (const breed of this.breedsList) {
-          const response = await axios.get(`/files/${breed.id}`);
-          this.images.push(response.data);
-        }
+        const response = await axios.get("files");
+        this.images = response.data;
       } catch (error) {
         console.error("Erro ao carregar Imagens", error);
       }
     },
     getBreedImage(breedId) {
       const image = this.images.find(img => img.ref_id_breed === breedId);
-      return image ? image.img : '';
+      return image ? `${axios.defaults.baseURL.replace('/api', '')}storage/${image.path}` : '';
+
+    },
+    async loadAllData() {
+      await this.loadBreeds();
+      await this.loadImages();
     }
   },
   mounted() {
-    this.loadBreeds();
-    this.loadImages();
+    this.loadAllData();
   }
 }
 </script>
