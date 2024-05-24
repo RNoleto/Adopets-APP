97eb03b3b3c87368a398b83cc8d42656eed32853
Ramon Noleto <devnoleto@gmail.com>
2024-05-24 13:04:48 -0300

    bkp
    ajuste da home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,27 +1,6 @@
 <template>
   <Banner />
-  <div class="container">
-    <section>
-      <div class="content">
-        <h3 class="title">Sobre a Adopets</h3>
-        <p class="text">
-          Somos mais do que apenas uma plataforma de adoção de animais; somos uma comunidade dedicada ao bem-estar dos
-          nossos amigos peludos e ao incentivo da adoção responsável de pets. Acreditamos que cada patinha merece um lar
-          amoroso e seguro, e estamos empenhados em fazer a diferença na vida desses animais e no mundo que
-          compartilhamos.
-        </p>
-      </div>
-    </section>
-    <section>
-      <div class="content">
-        <AdoptionList/>
-      </div>
-    </section>
-    <section>
-      <div class="content">
-        <PostsList />
-      </div>
-    </section>
+  <div class="content">
   </div>
 </template>
 <script>
@@ -38,17 +17,5 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.container {
-  padding-top: 50px;
 
-  .content {
-    .text {
-      margin-top: 15px;
-    }
-  }
-}
-
-@media (min-width: 600px) {}
-
-@media (min-width: 1210px) {}
 </style>
