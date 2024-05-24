f6337d6d0299b3308220ebd3f1c5f8edd402256d
Ramon Noleto <devnoleto@gmail.com>
2024-05-24 16:31:28 -0300

    AdoptionList
    Finalização do layout do componente AdoptionList para home

--- src/components/AdoptionList.vue
+++ src/components/AdoptionList.vue
@@ -7,13 +7,21 @@
         </div>
         <div class="pet_galery">
             <div v-for="animal in animalList" :key="animal.key" class="pet_card">
-                <img :src="animal.img" :alt="animal.name">
-                <p>{{ animal.name }}</p>
+                <img class="img_animal" :src="animal.img" :alt="animal.name">
+                <p class="card_title">{{ animal.name }}</p>
+                <div class="details">
+                    <span class="card_tag top"> <img :src="getGenderIcon(animal.sexo)" alt="icon" class="icon"> <p>{{ animal.sexo }}</p></span>
+                    <span class="card_tag"> <img src="../../public/assets/icons/event_available.svg" alt="icon" class="icon"> <p>{{ animal.idade }}</p></span>
+                    <span class="card_tag"> <img src="../../public/assets/icons/location.svg" alt="icon" class="icon"> <p>{{ animal.localizacao }}</p></span>
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import femaleIcon from '../../public/assets/icons/female.svg';
+import maleIcon from '../../public/assets/icons/male.svg';
+
 export default{
     data(){
         return{
@@ -22,25 +30,39 @@ export default{
                     id: 1,
                     img: "https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                     name: "Mingau",
+                    sexo: "Femea",
+                    idade: "2 anos",
+                    localizacao: "Centro, Itajaí - SC",
                 },
                 {
                     id: 2,
                     img: "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                     name: "Bolinha",
+                    sexo: "Macho",
+                    idade: "5 anos",
+                    localizacao: "Fazenda, Itajaí - SC",
                 },
                 {
                     id: 3,
                     img: "https://images.unsplash.com/photo-1574158622682-e40e69881006?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D",
                     name: "Felix",
+                    sexo: "Macho",
+                    idade: "3 anos",
+                    localizacao: "Dom Bosco, Itajaí - SC",
                 }
-                // {
-                //     id: 4,
-                //     img: "https://images.unsplash.com/photo-1615266508000-63ac219ac378?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                //     name: "Fred",
-                // }
             ]
         }
+    },
+    methods: {
+    getGenderIcon(sexo) {
+      if (sexo.toLowerCase() === "femea") {
+        return femaleIcon;
+      } else if (sexo.toLowerCase() === "macho") {
+        return maleIcon;
+      }
+      return '';
     }
+  }
 }
 </script>
 
@@ -58,6 +80,22 @@ export default{
         gap: 40px;
         .pet_card{
             max-width:360px;
+            .img_animal{
+                object-fit: cover;
+                max-height: 280px;
+            }
+            .details{
+                margin-top: 5px;
+                display: flex;
+                gap: 20px ;
+                row-gap: 5px;
+                justify-content: start;
+                flex-wrap: wrap;
+                .icon{
+                    width: 20px;
+                    height: 20px;
+                }
+            }
         }
     }
 }
