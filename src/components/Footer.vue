d8f05a815a436ff33dedd351b3922cb87b6e799c
Ramon Ribeiro Noleto <ramon.zap@hotmail.com>
2024-05-26 20:56:13 -0300

    Footer
    Ajuste basico do componente de footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,21 @@
   <footer>
     <div class="container">
       <div class="content">
-        <router-link to="/">Adopets</router-link>
+        <div class="column">
+          <router-link to="/" class="logo">Adopets</router-link>
+          <p class="copy">Adopets Alguns direitos reservados.</p>
+        </div>
+        <div class="column">
+          <p>Contato</p>
+          <p>devnoleto@gmail.com</p>
+        </div>
+        <div class="column">
+          <p>Informações</p>
+          <router-link to="/">Home</router-link>
+          <router-link to="#">Quem Somos</router-link>
+          <router-link to="#">Posts</router-link>
+          <router-link to="#">Achados e Perdidos</router-link>
+        </div>
       </div>
     </div>
   </footer>
@@ -16,10 +30,20 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-footer{
-  background-color: var(--color-3);
-  .content{
-    width: max-content;
+.container {
+  background-color: var(--p6);
+
+  .content {
+    display: flex;
+    padding: 120px 220px;
+    gap: 110px;
+
+    .column {
+      .copy {
+        margin-top: 20px;
+        color: var(--c4);
+      }
+    }
   }
 }
 </style>
