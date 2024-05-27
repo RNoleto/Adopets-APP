15552d1eaf0c2b9b2b90e7fde74253910535a38f
Ramon Ribeiro Noleto <ramon.zap@hotmail.com>
2024-05-26 22:38:58 -0300

    LostPet
    Criação do componente de achados e perdidos

---
+++ src/components/LostPet.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <p class="title">Achados e Perdidos.</p>
+      <p class="text_body">Esses pets estão perdidos e sozinhos em algum lugar, ajude a encontra-los.</p>
+      <button>Ver mais posts</button>
+      <div class="pets_galery">
+        <div class="card">
+          <img
+            src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhZ2xlfGVufDB8fDB8fHww"
+            alt="pet perdido">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background-color: var(--p5);
+
+  .content {
+    .text_body {
+      width: 500px;
+    }
+
+    .pets_galery {
+      display: flex;
+      gap: 40px;
+      margin-top: 20px;
+    }
+  }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <Banner />
-    <AboutUs />
-    <AdoptionList />
-    <PostsList />
-  </div>
+  <Banner />
+  <AboutUs />
+  <AdoptionList />
+  <PostsList />
+  <LostPet />
 </template>
 <script>
 import Banner from "@/components/Banner.vue";
 import AboutUs from "@/components/AboutUs.vue";
 import AdoptionList from "@/components/AdoptionList.vue";
 import PostsList from "@/components/PostsList.vue";
+import LostPet from "@/components/LostPet.vue";
 
 
 export default {
@@ -19,6 +19,7 @@ export default {
     AboutUs,
     AdoptionList,
     PostsList,
+    LostPet,
   },
 };
 </script>
