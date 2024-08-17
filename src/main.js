bc5a3127d5d4493079bc8a4d85ea5fc00022c192
Ramon Ribeiro Noleto <ramon.zap@hotmail.com>
2024-08-16 21:38:34 -0300

    Teste Vercel
    Teste com o endereço da vercel

--- src/main.js
+++ src/main.js
@@ -8,8 +8,10 @@ import axios from 'axios';
 const app = createApp(App);
 const pinia = createPinia();
 
-const storageBaseUrl = 'http://127.0.0.1:8000/storage/';
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
+// const storageBaseUrl = 'http://127.0.0.1:8000/storage/';
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
+const storageBaseUrl = 'https://api-nine-sigma-97.vercel.app/storage/';
+axios.defaults.baseURL = 'https://api-nine-sigma-97.vercel.app/api/';
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('_myapp_token')}`;
 
 axios.storageBaseUrl = storageBaseUrl;
