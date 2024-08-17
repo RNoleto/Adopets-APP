42e3556f420f6bc68aca024c1d72bfe3a2086c16
Ramon Ribeiro Noleto <ramon.zap@hotmail.com>
2024-08-17 17:13:01 -0300

    Vercel
    Teste com api em produção

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ const pinia = createPinia();
 
 // const storageBaseUrl = 'http://127.0.0.1:8000/storage/';
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
-const storageBaseUrl = 'https://api-nine-sigma-97.vercel.app/storage/';
-axios.defaults.baseURL = 'https://api-nine-sigma-97.vercel.app/api/';
+const storageBaseUrl = 'https://adopets-khvydfj7s-rnoletos-projects.vercel.app/storage/';
+axios.defaults.baseURL = 'https://adopets-khvydfj7s-rnoletos-projects.vercel.app/api/api/';
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('_myapp_token')}`;
 
 axios.storageBaseUrl = storageBaseUrl;
